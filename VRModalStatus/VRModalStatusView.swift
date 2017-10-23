6c6fc684d03b5de1c72f2d7c0987fb1ced830fb6
Varun Rathi <varunrathi28@gmail.com>
2017-10-23 16:03:01 +0530

    View setup for modal alert

--- VRModalStatus/VRModalStatusView.swift
+++ VRModalStatus/VRModalStatusView.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
-class VRModalStatusView: UIView {
+public class VRModalStatusView: UIView {
     
     
-    @IBOutlet weak var ivStatus:UIImageView!
-    @IBOutlet weak var lblHead:UILabel!
-    @IBOutlet weak var lblSubHead:UILabel!
+    @IBOutlet private weak var ivStatus:UIImageView!
+    @IBOutlet private weak var lblHead:UILabel!
+    @IBOutlet private weak var lblSubHead:UILabel!
 
+    let nibName = "VRModalStatusView"
+    var contentView:UIView!
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func set(image:UIImage)
+    {
+        self.ivStatus.image = image
+    }
+    
+    public func set(heading text:String)
+    {
+        self.lblHead.text = text
+    }
+    
+    func set(subHeading text:String)
+    {
+        self.lblSubHead.text = text
+    }
+    
+    func setUpView()
+    {
+        let bundle  = Bundle(for: type(of: self))
+        let nib  = UINib(nibName: nibName, bundle: bundle)
+        self.contentView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        addSubview(contentView)
+        
+        contentView.center = self.center
+        contentView.autoresizingMask = []
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        
+        lblHead.text = ""
+        lblSubHead.text = ""
+        
+        
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
