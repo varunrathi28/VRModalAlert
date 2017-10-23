d26c1e408b86fd8cc7ad993e3fec6b658ffdb8d8
Varun Rathi <varunrathi28@gmail.com>
2017-10-23 15:45:26 +0530

    Framework created

---
+++ VRModalStatus/VRModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  VRModalStatus.h
+//  VRModalStatus
+//
+//  Created by Varun Rathi on 23/10/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VRModalStatus.
+FOUNDATION_EXPORT double VRModalStatusVersionNumber;
+
+//! Project version string for VRModalStatus.
+FOUNDATION_EXPORT const unsigned char VRModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VRModalStatus/PublicHeader.h>
+
+
---
+++ VRModalStatus/VRModalStatusView.swift
@@ -0,0 +1,26 @@
+//
+//  VRModalStatusView.swift
+//  VRModalStatus
+//
+//  Created by Varun Rathi on 23/10/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+class VRModalStatusView: UIView {
+    
+    
+    @IBOutlet weak var ivStatus:UIImageView!
+    @IBOutlet weak var lblHead:UILabel!
+    @IBOutlet weak var lblSubHead:UILabel!
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
